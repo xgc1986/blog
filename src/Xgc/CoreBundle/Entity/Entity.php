0c77920ef4c2f265d69419fccc4b2813b57fd710
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2017-04-30 13:48:12 +0200

    Entities have last updated field

--- src/Xgc/CoreBundle/Entity/Entity.php
+++ src/Xgc/CoreBundle/Entity/Entity.php
@@ -2,11 +2,17 @@
 declare(strict_types=1);
 namespace Xgc\CoreBundle\Entity;
 
+use Xgc\UtilsBundle\Helper\DateTime;
 use Xgc\UtilsBundle\Helper\JSON;
 
 abstract class Entity implements JSON
 {
     protected $id;
+
+    /**
+     * @var DateTime
+     */
+    protected $updatedAt;
 
     function __toArray(): array
     {
@@ -21,5 +27,26 @@ abstract class Entity implements JSON
     public function getId(): int
     {
         return $this->id;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTime $updatedAt
+     */
+    public function setUpdatedAt(DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public final function refresh()
+    {
+        $this->updatedAt = new DateTime();
     }
 }
