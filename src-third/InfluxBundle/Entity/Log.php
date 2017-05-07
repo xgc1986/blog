06b2e8fe93ce7be4890619614f8205aa7de0b725
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2017-05-08 00:31:39 +0200

    Added logger

---
+++ src-third/InfluxBundle/Entity/Log.php
@@ -0,0 +1,119 @@
+<?php
+declare(strict_types=1);
+namespace Xgc\InfluxBundle\Entity;
+
+use Xgc\InfluxBundle\Annotation\Measurement;
+use Xgc\InfluxBundle\Annotation\Tag;
+use Xgc\InfluxBundle\Entity\MeasurementEntity;
+use Xgc\UtilsBundle\Helper\DateTime;
+
+/**
+ * @Measurement("log")
+ */
+class Log extends MeasurementEntity
+{
+    /**
+     * @Tag
+     */
+    protected $tag;
+
+    /**
+     * @Tag
+     */
+    protected $username;
+
+    /**
+     * @Tag
+     */
+    protected $level;
+
+    /**
+     * @Tag
+     */
+    protected $message;
+
+    public function __construct(?DateTime $date = null)
+    {
+        parent::__construct($date);
+    }
+
+    public static function create(string $tag, string $user, string $type, string $message = ''): Log
+    {
+        $ret = new Log();
+        $ret->setLevel($type);
+        $ret->setMessage($message);
+        $ret->setTag($tag);
+        $ret->setUsername($user);
+
+        return $ret;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLevel(): string
+    {
+        return $this->level;
+    }
+
+    /**
+     * @param string $level
+     */
+    public function setLevel(string $level): void
+    {
+        $this->level = $level;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTag(): string
+    {
+        return $this->tag;
+    }
+
+    /**
+     * @param string $tag
+     */
+    public function setTag(string $tag)
+    {
+        $this->tag = $tag;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function __getType(): string
+    {
+        return 'log';
+    }
+}
---
+++ src-third/InfluxBundle/Service/Logger.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+namespace Xgc\InfluxBundle\Service;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Xgc\CoreBundle\Entity\User;
+use Xgc\InfluxBundle\Entity\Log;
+
+class Logger
+{
+    const INFO = "INFO";
+    const DEBUG = "DEBUG";
+    const WARNING = "WARNING";
+    const ERROR = "ERROR";
+
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function info(?User $user, string $tag, string $message): Log
+    {
+        return $this->log(self::INFO, $user, $tag, $message);
+    }
+
+    public function debug(?User $user, string $tag, string $message): Log
+    {
+        return $this->log(self::DEBUG, $user, $tag, $message);
+    }
+
+    public function warning(?User $user, string $tag, string $message): Log
+    {
+        return $this->log(self::WARNING, $user, $tag, $message);
+    }
+
+    public function error(?User $user, string $tag, string $message): Log
+    {
+        return $this->log(self::ERROR, $user, $tag, $message);
+    }
+
+    private function log(string $level, ?User $user, string $tag, string $message): Log
+    {
+        $log = new Log();
+        $log->setUsername($user ? $user->getUsername() : '');
+        $log->setLevel($level);
+        $log->setMessage($message);
+        $log->setTag($tag);
+
+        $influx = $this->container->get('xgc.influx');
+        $influx->write([$log]);
+
+        return $log;
+    }
+
+}
