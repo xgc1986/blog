bd05bf09f2afd912bdaa2f79c0a7b4ed366b5f85
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2017-07-08 19:17:33 +0200

    add validation

--- src-third/CoreBundle/Entity/Entity.php
+++ src-third/CoreBundle/Entity/Entity.php
@@ -3,12 +3,19 @@ declare(strict_types=1);
 namespace Xgc\CoreBundle\Entity;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Xgc\UtilsBundle\Helper\DateTime;
 use Xgc\UtilsBundle\Helper\JSON;
 
 abstract class Entity implements JSON
 {
     protected $id;
+
+    /**
+     * @var DateTime
+     * @Assert\DateTime()
+     */
+    protected $createdAt;
 
     /**
      * @var DateTime
@@ -34,6 +41,22 @@ abstract class Entity implements JSON
     /**
      * @return DateTime
      */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return DateTime
+     */
     public function getUpdatedAt(): DateTime
     {
         return $this->updatedAt;
@@ -47,8 +70,32 @@ abstract class Entity implements JSON
         $this->updatedAt = $updatedAt;
     }
 
-    public final function refresh()
+    public function __prePersist()
     {
-        $this->updatedAt = new DateTime();
+        $this->setCreatedAt(new DateTime());
+        $this->setUpdatedAt(new DateTime());
+    }
+
+    public function __preUpdate()
+    {
+        $this->setUpdatedAt(new DateTime());
+    }
+
+    /**
+     * @Assert\Callback()
+     * @param ExecutionContextInterface $context
+     * @param $payload
+     */
+    public function __validate(ExecutionContextInterface $context, $payload)
+    {
+        $this->validate($context, $payload);
+    }
+
+    /**
+     * @param $context
+     * @param $payload
+     */
+    public function validate(ExecutionContextInterface $context, $payload)
+    {
     }
 }
